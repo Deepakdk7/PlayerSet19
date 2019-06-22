90c643b7d246158c4c015f009a89589be5177d9e
Deepakdk7 <51220607+Deepakdk7@users.noreply.github.com>
2019-06-22 21:46:39 +0530

    Create 192.py

---
+++ 192.py
@@ -0,0 +1,10 @@
+ax=int(input())
+a=list(map(int,input().split()))
+for i in range(0,ax-1):
+    if i%2==0 and a[i]>=a[i+1]:
+        print('no')
+        break
+    elif i%2!=0 and a[i]<=a[i-1]:
+        print('no')
+else:
+    print('yes')
