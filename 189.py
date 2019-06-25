786ddf1a6cc0b5566be551582982aaa167e1c2f7
Deepakdk7 <51220607+Deepakdk7@users.noreply.github.com>
2019-06-25 16:25:28 +0530

    Update 189.py

--- 189.py
+++ 189.py
@@ -1,5 +1,5 @@
 x,y,z=list(map(int,input().split()))
-if (x+y)>z and (x+z)>y and (y+z)>x:
+if (x+y)>z and (x+z)>y and (y+z)>x and a!=b and a!=c and a!=b!=c:
     print('yes')
 else:
     print('no')
